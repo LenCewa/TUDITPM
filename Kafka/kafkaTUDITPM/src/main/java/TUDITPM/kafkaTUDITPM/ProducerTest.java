368e8990839cae15a7dffb2a6438a2ccb5f0b49d
iBuaRecords <daywalker@pferr.de>
2016-11-12 23:22:31 +0100

    Add files via upload
    Kafka Test:
    1. Über cmd zookeeper starten
    2. Über cmd kafka starten
    3. In eclipse producer und consumer starten 
    4. In test.txt neue zeile hinzufügen diese wird dann als neue Nachricht an den Consumer geschickt

---
+++ Kafka/kafkaTUDITPM/src/main/java/TUDITPM/kafkaTUDITPM/ProducerTest.java
@@ -0,0 +1,54 @@
+package TUDITPM.kafkaTUDITPM;
+
+import java.util.List;
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+public class ProducerTest {
+
+  public static void main(String[] args) {
+    Properties props = new Properties();
+    props.put("bootstrap.servers", "localhost:9092");
+    props.put("acks", "all");
+    props.put("retries", 0);
+    props.put("batch.size", 16384);
+    props.put("linger.ms", 1);
+    props.put("buffer.memory", 33554432);
+    props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+    props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+    Producer<String, String> producer = null;
+    try {
+      producer = new KafkaProducer<>(props);
+      String message = ReadFile.getMessage(null);
+      boolean first = true;
+      while(true){
+    	  String m = ReadFile.getMessage(null);
+    	  if(!m.equals(message) || first){
+    		  String newMessage = m.substring(message.length(), m.length());
+    		  System.out.println(m + " new: " + newMessage);
+    		  if(!first)
+    			  producer.send(new ProducerRecord<String, String>("TestKafkaTopic", newMessage));
+    		  else
+    			  producer.send(new ProducerRecord<String, String>("TestKafkaTopic", m));
+    		  message = m;
+    		  first = false;
+    	  }
+      }
+//      for (int i = 0; i < 100; i++) {
+//        String msg = "Message " + i;
+//        producer.send(new ProducerRecord<String, String>("TestKafkaTopic", msg));
+//        System.out.println("Sent:" + msg);
+//      }
+    } catch (Exception e) {
+      e.printStackTrace();
+
+    } finally {
+      producer.close();
+    }
+
+  }
+
+}
---
+++ Kafka/kafkaTUDITPM/src/main/java/TUDITPM/kafkaTUDITPM/ReadFile.java
@@ -0,0 +1,32 @@
+package TUDITPM.kafkaTUDITPM;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class ReadFile {
+
+	
+	private static List<String> readLines(){
+		
+		List<String> lines = null;
+		try {
+			lines = Files.readAllLines(Paths.get("test.txt"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return lines;
+	}
+	
+	public static String getMessage(String keyword){
+		
+		StringBuilder sb = new StringBuilder();
+		for(String s : readLines()){
+			sb.append(s);
+		}
+		return sb.toString();
+	}
+}
