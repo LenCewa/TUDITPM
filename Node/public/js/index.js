11da5577b7c13879208771b8267d068c5fea7b43
iBuaRecords <daywalker@pferr.de>
2016-11-30 11:46:48 +0100

    Erster Ansatz Datenanzeige

--- Node/public/js/index.js
+++ Node/public/js/index.js
@@ -1,24 +1,46 @@
+// public/js/index.js
 /**
  * Javascript file for all the funtions used in the configuration section.
  * 
  * @author       Yannick Pferr
  * @author       Tobias Mahncke
  * 
- * @version      2.1
+ * @version      2.2
  */
 
 /**
  * Sends the company given in the input field "companyName" to the server.
  */
 function postCompany() {
-	alert('Click ' + $('#companyName').val());
 	$.ajax({
 		type: 'POST',
 		url: '/api/company',
 		data: '{"company":"' + $('#companyName').val() + '"}',
 		success: function(data) {
 			// TODO: reload list of companies
+			$.get("/api/company", function(data){
+				showCompanies(data);
+			});
 		},
 		contentType: 'application/json'
 	});
+}
+
+function showCompanies(){
+	
+	var table = document.getElementById("companyTable");
+	var array = data.split("\n");
+	for (var i = 0; i < 3; i++){
+		var tr = document.createElement('tr');   
+
+		var td1 = document.createElement('td');
+
+		var text1 = document.createTextNode(array[i]);
+
+		td1.appendChild(text1);
+		tr.appendChild(td1);
+		
+		table.appendChild(tr);
+	}
+	document.body.appendChild(table);
 }
