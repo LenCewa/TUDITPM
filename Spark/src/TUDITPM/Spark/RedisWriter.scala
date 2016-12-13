6e109de4a71e3fcb41b64b58d8e47b0480cf12c5
LycketySplit <lucky.koch@gmx.de>
2016-12-13 15:00:43 +0100

    Kommentiert

--- Spark/src/TUDITPM/Spark/RedisWriter.scala
+++ Spark/src/TUDITPM/Spark/RedisWriter.scala
@@ -6,23 +6,34 @@ import scala.util.parsing.json.JSONObject
 import com.mongodb.casbah.commons.MongoDBObject
 import redis.clients.jedis.Jedis
 
-
+/**
+ * Stores collection items in Redis
+ *
+ * @author Ludwig Koch
+ * @version 3.0
+ */
 
 
 object RedisWriter {
+  //Open connection to MongoDB
   val mongoConn = MongoClient("localhost", 27017)
   val mongoDB = mongoConn("dbtest")
   val mongoColl = mongoDB("testcollection")
   
   def writeToRedis(){
+    //Loads all elements in the collection
     var it = mongoColl.find()
+    //Creates new JSONArray with all elements stored in it
     var news = new JSONArray(it.toList)
-    
+   
+    //Builds new JSONObject
     val builder = MongoDBObject.newBuilder
     builder += "Meldungen" -> news
     val company = builder.result
+    //Converts JSONObject to JSONString
     val result = company.toString()
     
+    //Opens Redis connection and stores the JSONString
     val jedis = new Jedis("localhost")
     jedis.set("TestKey", result)
     println("Stored string : " + jedis.get("TestKey"));
